c2fc109c017f8bec61ab8a8d567a665752c1888d
Juan Tomas Ortiz <juantortiz@gmail.com>
2020-12-04 00:29:02 -0300

    ansible_install

---
+++ install_ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#### INSTALL ANSIBLE CENTOS 8 CORE - MINIMAL INSTALL
+
+dnf update -y 
+
+#### INSTALL EPEL release
+
+dnf install -y epel-release
+dnf install -y ansible expect
+
+
+##### generate ssh key for the hosts
+
+ssh-keygen -q -b 4096 -t rsa -N '' -f ./id_rsa
+
