8680932655468c7a00d120b46d78e519cdacdf09
Juan Tomas Ortiz <juantortiz@gmail.com>
2020-12-10 13:45:44 -0300

    new commit

--- basrc_alias.sh
+++ basrc_alias.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+cp .bashrc .bashrcoriginal
+echo ""> .bashrc
 ### virsh alias
 echo "alias lsvirt='virsh list --all'" >> .bashrc
 echo "alias lsvirtnet='virsh net-list'" >> .bashrc
 
 ### aplicar cambios
+cat .bashrcoriginal >> .bashrc
 . .bashrc
 
 
